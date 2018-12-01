2e2deca8217597c3fa905c66cc1db1e47eb514cf
mathix <mathixx@gmail.com>
2018-12-01 12:34:07 +0100

    Receiving sub notification

--- telegram_bot/notifier_bot.py
+++ telegram_bot/notifier_bot.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from telegram import Update, Bot, User
 from telegram.ext import Updater, CommandHandler
 
 
@@ -13,10 +16,19 @@ class NotifierBot:
     def help_command(self, bot, update):
         update.message.reply_text('Help!')
 
+    def sub_command(self, bot : Bot, update : Update, args: List[str]):
+        if len(args) == 1:
+            print('subscribe!')
+            user: User = update.effective_user
+            update.message.reply_text(f'Hello {user.full_name}')
+        else:
+            update.message.reply_text('usage:\n/sub [todoist_api_key]')
+
     def start(self):
         dp = self._updater.dispatcher
 
         dp.add_handler(CommandHandler('help', self.help_command))
+        dp.add_handler(CommandHandler('sub', self.sub_command, pass_args=True))
 
         self._updater.start_polling()
         self._updater.idle()
